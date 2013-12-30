8072b0e723686827cf737f2ba1dde62897857021
tommyrojo <tommyrojo@gmail.com>
2013-12-29 18:18:52 -0800

    adding mostly final version of support page

---
+++ home/js/common-support.js
@@ -0,0 +1,25 @@
+jQuery.noConflict();
+jQuery(document).ready(function($){
+	$("#index").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home";
+	})
+	$("#news").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/about/news.html";
+	})
+	$("#software").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/fibersim/fibersim.html";
+	})
+	$("#training").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+	})
+	$("#resources").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/resources/resources.html";
+	})
+	$("#support").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/support/support.html";
+	})
+	$("#contact-us").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/contact/contact.html";
+	})
+
+});
