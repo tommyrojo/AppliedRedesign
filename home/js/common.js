94176b4fe67bb08249fcfd5e0a2d1b6e5201c5fb
tommyrojo <tommyrojo@gmail.com>
2013-12-30 22:57:21 -0800

    adding menu changes to index for now

--- home/js/common.js
+++ home/js/common.js
@@ -5,10 +5,52 @@ $(function(){
 	$("#news").click(function(){
 		document.location.href = "http://sandbox.appliedcax.com/home/about/news.html";
 	})
-	$("#software").click(function(){
+	$("#story").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/about/ourstory.html";
+	})
+	$("#clients").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/about/ourclients.html";
+	})
+	$("#bct").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/bct/bct.html";
+	})
+	$("#femap").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/femap/femap.html";
+	})
+	$("#fibersim").click(function(){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/fibersim/fibersim.html";
 	})
+	$("#nx").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nx.html";
+	})
+	$("#nxfordesign").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxfordesign.html";
+	})
+	$("#nxformanufacturing").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxformanufacturing.html";
+	})
+	$("#nxforsimulation").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxforsimulation.html";
+	})
+	$("#cnc").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/postsimkits/cnc.html";
+	})
+	$("#teamcenterplm").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/teamcenter/teamcenterplm.html";
+	})
 	$("#training").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+	})
+	$("#training").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+	})
+	$("#cad").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+	})
+	$("#cam").click(function(){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+	})
+	$("#cae").click(function(){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
 	})
 	$("#resources").click(function(){
