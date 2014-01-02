44b187ac2a26d08e834391dc6cf0922780801cc4
tommyrojo <tommyrojo@gmail.com>
2014-01-01 18:39:51 -0800

    adding menu to all pages. IE compatible

--- home/js/common-support.js
+++ home/js/common-support.js
@@ -1,25 +1,85 @@
 jQuery.noConflict();
 jQuery(document).ready(function($){
-	$("#index").click(function(){
+	$("#index").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home";
 	})
-	$("#news").click(function(){
+	$("#news").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/about/news.html";
 	})
-	$("#software").click(function(){
+	$("#story").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/about/ourstory.html";
+		e.stopImmediatePropagation();
+	})
+	$("#clients").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/about/ourclients.html";
+		e.stopImmediatePropagation();
+	})
+	$("#bct").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/bct/bct.html";
+		e.stopImmediatePropagation();
+	})
+	$("#femap").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/femap/femap.html";
+		e.stopImmediatePropagation();
+	})
+	$("#fibersim").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/fibersim/fibersim.html";
+		e.stopImmediatePropagation();
 	})
-	$("#training").click(function(){
+	$("#nx").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nx.html";
+		e.stopImmediatePropagation();
+	})
+	$("#nxfordesign").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxfordesign.html";
+		e.stopImmediatePropagation();
+	})
+	$("#nxformanufacturing").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxformanufacturing.html";
+		e.stopImmediatePropagation();
+	})
+	$("#nxforsimulation").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxforsimulation.html";
+		e.stopImmediatePropagation();
+	})
+	$("#cnc").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/postsimkits/cnc.html";
+		e.stopImmediatePropagation();
+	})
+	$("#teamcenterplm").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/software/teamcenter/teamcenterplm.html";
+		e.stopImmediatePropagation();
+	})
+	$("#training").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
 	})
-	$("#resources").click(function(){
+	$("#training").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
+	})
+	$("#cad").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
+	})
+	$("#cam").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
+	})
+	$("#cae").click(function(e){
+		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
+	})
+	$("#resources").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/resources/resources.html";
+		e.stopImmediatePropagation();
 	})
-	$("#support").click(function(){
+	$("#support").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/support/support.html";
+		e.stopImmediatePropagation();
 	})
-	$("#contact-us").click(function(){
+	$("#contact-us").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/contact/contact.html";
+		e.stopImmediatePropagation();
 	})
-
 });
--- home/js/common.js
+++ home/js/common.js
@@ -1,66 +1,85 @@
 $(function(){ 
-	$("#index").click(function(){
+	$("#index").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home";
 	})
-	$("#news").click(function(){
+	$("#news").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/about/news.html";
 	})
-	$("#story").click(function(){
+	$("#story").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/about/ourstory.html";
+		e.stopImmediatePropagation();
 	})
-	$("#clients").click(function(){
+	$("#clients").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/about/ourclients.html";
+		e.stopImmediatePropagation();
 	})
-	$("#bct").click(function(){
+	$("#bct").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/bct/bct.html";
+		e.stopImmediatePropagation();
 	})
-	$("#femap").click(function(){
+	$("#femap").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/femap/femap.html";
+		e.stopImmediatePropagation();
 	})
-	$("#fibersim").click(function(){
+	$("#fibersim").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/fibersim/fibersim.html";
+		e.stopImmediatePropagation();
 	})
-	$("#nx").click(function(){
+	$("#nx").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nx.html";
+		e.stopImmediatePropagation();
 	})
-	$("#nxfordesign").click(function(){
+	$("#nxfordesign").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxfordesign.html";
+		e.stopImmediatePropagation();
 	})
-	$("#nxformanufacturing").click(function(){
+	$("#nxformanufacturing").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxformanufacturing.html";
+		e.stopImmediatePropagation();
 	})
-	$("#nxforsimulation").click(function(){
+	$("#nxforsimulation").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/nx/nxforsimulation.html";
+		e.stopImmediatePropagation();
 	})
-	$("#cnc").click(function(){
+	$("#cnc").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/postsimkits/cnc.html";
+		e.stopImmediatePropagation();
 	})
-	$("#teamcenterplm").click(function(){
+	$("#teamcenterplm").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/software/teamcenter/teamcenterplm.html";
+		e.stopImmediatePropagation();
 	})
-	$("#training").click(function(){
+	$("#training").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
 	})
-	$("#training").click(function(){
+	$("#training").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
 	})
-	$("#cad").click(function(){
+	$("#cad").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
 	})
-	$("#cam").click(function(){
+	$("#cam").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
 	})
-	$("#cae").click(function(){
+	$("#cae").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/training/training.html";
+		e.stopImmediatePropagation();
 	})
-	$("#resources").click(function(){
+	$("#resources").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/resources/resources.html";
+		e.stopImmediatePropagation();
 	})
-	$("#support").click(function(){
+	$("#support").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/support/support.html";
+		e.stopImmediatePropagation();
 	})
-	$("#contact-us").click(function(){
+	$("#contact-us").click(function(e){
 		document.location.href = "http://sandbox.appliedcax.com/home/contact/contact.html";
+		e.stopImmediatePropagation();
 	})
 
 });
