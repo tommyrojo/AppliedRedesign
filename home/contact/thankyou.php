9dbe8bee930abe0c494ca7442abe243da07f3ede
nwdesignflow <james.mcpherson@appliedcax.com>
2014-01-08 10:06:38 -0800

    google analytics code

--- home/contact/thankyou.php
+++ home/contact/thankyou.php
@@ -105,6 +105,19 @@ mail($Toemail, $Category, $MessageText, $header);
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Applied CAx</title>
 <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7721704-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 
